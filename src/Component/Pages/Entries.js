2a3754f433043b6aa9fbfa376af312a45aa7ddd3
HeidiG12 <hguntert@ufl.edu>
2024-04-18 15:29:42 -0400

    working on friend page

--- src/Component/Pages/Entries.js
+++ src/Component/Pages/Entries.js
@@ -4,8 +4,14 @@ import React from "react";
 
 const Entries = () => {
   return (
-    <div>
-        <p>entries</p>
+    <div className="home">
+      <div className="header">
+        <div className="logo1"></div>
+      </div>
+      <div className="about">
+        <h2>Your Reviews</h2>
+        <p>hello</p>
+      </div>
     </div>
   );
 };
--- src/Component/Pages/Friends.js
+++ src/Component/Pages/Friends.js
@@ -1,18 +1,55 @@
 import React from "react";
 import "../StyleSheets/Friends.css";
-
+import pic1 from "../imgs/harrys.png";
+import pic2 from "../imgs/swamp.png";
+import pic3 from "../imgs/maudes.png";
+import pic4 from "../imgs/satchel.png";
+import pic5 from "../imgs/bistro.png";
+import pic6 from "../imgs/bolay.png";
+import pic7 from "../imgs/luke.png";
+import pic8 from "../imgs/apa.png";
 
 const Friends = () => {
   return (
     <div className="home">
-      <div className="header">
-        <div className="logo1"></div>
-      </div>
       <div className="about">
         <h2>Your Friends</h2>
-        <p>also if no friends at the start add "Search and Follow friends to see them here" then once have friends get rid of text</p>
-        <p>need to write code to add friends as they follow them</p>
-      
+        <div className="homePictures">
+          <button className="pic">
+            <img src={pic1} alt="Harry's" />
+            <div class="card-text">Joyin</div>
+          </button>
+          <button className="pic">
+            <img src={pic3} alt="Maudes" />
+            <div class="card-text">Tina</div>
+          </button>
+          <button className="pic">
+            <img src={pic2} alt="Swamp" />
+            <div class="card-text">Heidi</div>
+          </button>
+          <button className="pic">
+            <img src={pic4} alt="Satchel" />
+            <div class="card-text">Jess</div>
+          </button>
+        </div>
+        <div className="homePictures">
+          <button className="pic">
+            <img src={pic5} alt="Bistro" />
+            <div class="card-text">Tenders</div>
+          </button>
+          <button className="pic">
+            <img src={pic6} alt="Bolay" />
+            <div class="card-text">Albert</div>
+          </button>
+          <button className="pic">
+            <img src={pic7} alt="Luke" />
+            <div class="card-text">Alberta</div>
+          </button>
+          <button className="pic">
+            <img src={pic8} alt="Apa" />
+            <div class="card-text">Gator</div>
+          </button>
+        </div>
       </div>
     </div>
   );
