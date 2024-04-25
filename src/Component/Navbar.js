dfcb0a3b8078b35c57fcd90583e7a6e4fa34fa24
jessleathe <116766177+jessleathe@users.noreply.github.com>
2024-04-24 21:16:20 -0400

    Cleaned code and added comments

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -7,33 +7,39 @@ import types from "./imgs/dif_types.png";
 import profileIcon from "./imgs/profileIcon.png";
 
 function Navbar() {
+  // State to handle the toggle of the menu
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  // State to track and set the active menu item
   const [activeItem, setActiveItem] = useState(() => {
     return localStorage.getItem('activeItem') || null;
   });
 
+  // Effect to store the active item in localStorage when it changes
   useEffect(() => {
     localStorage.setItem('activeItem', activeItem);
   }, [activeItem]);
 
+  // Function to handle item click, sets the active item and color
   const handleItemClick = (itemName) => {
     setActiveItem(itemName)
     setColor(itemName);
   };
 
+  // Function to set the color, currently only returns red color
   const setColor = (itemName) => ({
     color:'red'
   });
 
+  // Profile SVG icon setup
   const profileSVG = (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
       <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-2 3-6 3s-6-2-6-3c0-1 2-3 6-3s6 2 6 3zm-1-.004c-.29-.997-1.826-2.996-5-2.996s-4.71 2-5 2.996C4.29 12.999 5.826 11 9 11s4.71 2 5 2.996z"/>
     </svg>
-  
   );
 
+  // The component renders the navigation bar with a responsive design.
   return (
-
     //start of the nav bar
     <div style={{ maxWidth: '100%', 'padding-left':'100px','padding-right ':'100px','padding-bottom':'10px'}}> {/* This div acts as the container */}
       
@@ -53,8 +59,7 @@ function Navbar() {
           </Link>        
         </div>
       </div>
-
-
+          
       <div style={{ display: 'flex', 'padding-left':'100px','padding-right':'100px' }}> {/* Second row */}
         <div style={{ width: '20%' }}>
           <Link to="/entries" className="nav-link">Entries</Link>
@@ -69,12 +74,9 @@ function Navbar() {
           <Link to="/search" className="nav-link">Search</Link>
         </div>
       </div>
-
-
-
     </div>
   );
 }
 
-
+// Export the Navbar component
 export default Navbar
