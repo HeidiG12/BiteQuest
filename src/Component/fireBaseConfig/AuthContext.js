729e03a1bf6ee67e1331f9e5197a7d15dc2bbc40
jessleathe <116766177+jessleathe@users.noreply.github.com>
2024-04-24 21:17:31 -0400

    Cleaned up code and added comments

--- src/Component/fireBaseConfig/AuthContext.js
+++ src/Component/fireBaseConfig/AuthContext.js
@@ -2,16 +2,21 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../fireBaseConfig/OAuth'; // Import your Firebase auth
 
+// Create a context for authentication data
 const AuthContext = createContext();
 
+// Custom hook to use the auth context value
 export function useAuth() {
   return useContext(AuthContext);
 }
 
+// Provider component that manages the authentication state
 export const AuthProvider = ({ children }) => {
+  // State to hold the current user
   const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
+    // Listen for changes to the user's authentication status
     const unsubscribe = onAuthStateChanged(auth, user => {
       setCurrentUser(user);
     });
@@ -19,6 +24,7 @@ export const AuthProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
+  // Provide the currentUser in the context for access throughout the application
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
