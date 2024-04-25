f206ed45ff8c9a5abe8deb5135d584df74673fd4
jessleathe <116766177+jessleathe@users.noreply.github.com>
2024-04-24 21:12:46 -0400

    Cleaned code and added comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,21 @@
-//import { render, screen } from '@testing-library/react';
 import App from './App';
-
-/*test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});*/
-
 import {displaySelected, myDisplay, getData, checkingTagsSelected, tagSearch, mixingSort, sortingResults} from './tags.js';
+
+// Define a test case to check if the correct restaurants are returned based on tag frequency.
 test('Does it return correct restaurants by frequency of tags', async() => {
   let actual = await tagSearch(['Spicy', 'Sweet']);
   let expected = ['16TH AVENUE DINER', 'APPLEBEE\'S', 'ASIAN WOK & GRILL'];
   expect(actual).toEqual(expected);
 })
+
+// Define a test case to check if no restaurants are returned when no tags match.
 test('Does sstem output nothing when there are no restaurants that fit tag preference', async() => {
   let actual = await tagSearch(['Spicy', 'Sour']);
   let expected = [];
   expect(actual).toEqual(expected);
 })
+
+// Define a test case to handle the edge case when no tags are entered.
 test('Edge Case: No tags are entered', async() => {
   let actual = await tagSearch([]);
   let expected = [];
